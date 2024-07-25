97825b62c30d771be8a1b08fcac66430b49e129a
Victoria <oyelolaabimbimbola23@gmail.com>
2024-07-25 20:41:50 +0100

    adjust app wrapper style

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import Hero from "./components/Hero"
 function App() {
 
   return (
-    <div className="relative w-screen min-h-screen overflow-hidden">
-      <div className="absolute z-[1] left-0 top-0 min-w-[1440px] h-full site-bg"></div>
+    <div className="relative w-screen min-h-screen overflow-hidden bg-black">
+      <div className="absolute inset-0 mx-auto z-[1] left-0 top-0 min-w-[1440px] h-full site-bg"></div>
       <Header />
       <main>
         <Hero />
