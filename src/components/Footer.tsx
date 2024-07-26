a7ef6ca76bf08808cae92c4a258188441a7ea9ab
Victoria <oyelolaabimbimbola23@gmail.com>
2024-07-26 18:54:11 +0100

    add aria labels to links wrapping logos

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,8 +6,8 @@ export default function Footer(): JSX.Element {
             <div className="flex flex-col md:flex-row md:items-center justify-between gap-5 bg-black rounded-2xl p-5 md:py-5 md:px-10 ">
                 <p className="order-3 md:order-1 font-satoshi font-medium text-sm md:text-base text-transparent bg-clip-text bg-metallic-text"><span className="font-bold">©</span> <a className="bg-cta-primary bg-clip-text hover:underline hover:border-b hover:border-off-white" href="https://linkedin.com/in/victoriaoyelola" target="_blank">Oyelola Victoria.</a> All rights reserved.</p>
                 <ul className="order-1 md:order-2 flex gap-4 md:gap-8">
-                    <li><a className="block first:w-7 first:h-7" href="https://linkedin.com/in/victoriaoyelola" target="_blank">{logos.linkedin}</a></li>
-                    <li><a className="block first:w-7 first:h-7" href="https://github.com/VriaA" target="_blank" >{logos.github}</a></li>
+                    <li><a className="block first:w-7 first:h-7" href="https://linkedin.com/in/victoriaoyelola" aria-label="Link to Oyelola Victoria's LinkedIn profile" target="_blank">{logos.linkedin}</a></li>
+                    <li><a className="block first:w-7 first:h-7" href="https://github.com/VriaA" aria-label="Link to Oyelola Victoria's GitHub profile" target="_blank" >{logos.github}</a></li>
                 </ul>
                 <p className="order-2 md:order-3 font-satoshi font-medium text-sm md:text-base text-transparent bg-clip-text bg-metallic-text">Designed & developed by <a className="bg-cta-primary bg-clip-text hover:underline hover:border-b hover:border-off-white" href="https://github.com/VriaA" target="_blank">Oyelola Victoria.</a></p>
             </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,8 +30,8 @@ export default function Header(): JSX.Element {
         <div className="flex items-center gap-4 md:gap-8">
             <nav>
                 <ul className="flex gap-4 md:gap-8">
-                    <li><a className="block first:w-7 first:h-7" href="https://linkedin.com/in/victoriaoyelola" target="_blank">{logos.linkedin}</a></li>
-                    <li><a className="block first:w-7 first:h-7" href="https://github.com/VriaA" target="_blank" >{logos.github}</a></li>
+                    <li><a className="block first:w-7 first:h-7" href="https://linkedin.com/in/victoriaoyelola" aria-label="Link to Oyelola Victoria's LinkedIn profile" target="_blank">{logos.linkedin}</a></li>
+                    <li><a className="block first:w-7 first:h-7" href="https://github.com/VriaA" aria-label="Link to Oyelola Victoria's GitHub profile" target="_blank" >{logos.github}</a></li>
                 </ul>
             </nav>
 
