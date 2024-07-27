de1ae218348dd6238e09bb4664844458462ec414
VriaA <123843734+VriaA@users.noreply.github.com>
2024-07-27 23:43:21 +0100

    adjust background grid position

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <div className="relative w-full overflow-hidden bg-black px-5 md:px-20">
-      <div className="absolute inset-0 mx-auto z-[1] left-0 top-0 min-w-[1440px] h-full site-bg"></div>
+      <div className="absolute inset-0 mx-auto z-[1] left-[-1px] top-[-1px] min-w-[1440px] h-full site-bg"></div>
       <AppContextProvider>
         <Header />
         <main className="flex flex-col gap-20">
