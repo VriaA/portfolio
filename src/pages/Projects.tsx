1acd3b2547514c0530d9c07de47ccc8fcd652ec2
VriaA <oyelolaabimbola23@gmail.com>
2024-10-16 22:22:10 +0100

    adjust spacing in project page

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -4,7 +4,7 @@ import Contact from "@/components/Contact"
 
 export default function Projects(): JSX.Element {
     return (
-        <div className="pt-16">
+        <div className="mt-10 md:mt-20 flex flex-col gap-20">
             <ProjectsComponent projects={projects} />
             <Contact />
         </div>
