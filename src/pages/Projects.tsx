f4449697050a7d9940761b810632d83222a9b2b1
VriaA <oyelolaabimbola23@gmail.com>
2024-10-16 22:13:08 +0100

    add top padding to project page

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -4,9 +4,9 @@ import Contact from "@/components/Contact"
 
 export default function Projects(): JSX.Element {
     return (
-        <>
+        <div className="pt-16">
             <ProjectsComponent projects={projects} />
             <Contact />
-        </>
+        </div>
     )
 }
