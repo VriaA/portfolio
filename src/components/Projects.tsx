e9d2050a6abf131f98abf0e05977fc10dc2228bc
VriaA <oyelolaabimbola23@gmail.com>
2024-10-29 02:38:54 +0100

    add hover effect to right arrow icon

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -86,8 +86,8 @@ export default function Projects({ projects }: { projects: Project[] }): JSX.Ele
 
             {
                 !isProjectsPage &&
-                <Link to="/projects" className="flex items-center gap-2 mt-10 border-b border-b-white pb-1 font-satoshi font-medium text-lg lg:text-xl text-transparent bg-clip-text bg-cta-primary tracking-[0.01em]"><ShinyText text="See all projects" speed={1} />
-                    <svg className="w-5 animate-right" fill="#AEB2FA" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" aria-label="Right arrow icon">
+                <Link to="/projects" className="group flex items-center gap-2 mt-10 border-b border-b-white pb-1 font-satoshi font-medium text-lg lg:text-xl text-transparent bg-clip-text bg-cta-primary tracking-[0.01em]"><ShinyText text="See all projects" speed={1} />
+                    <svg className="w-5 animate-right group-hover:animate-none" fill="#AEB2FA" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" aria-label="Right arrow icon">
                         <path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z" />
                     </svg>
                 </Link>
