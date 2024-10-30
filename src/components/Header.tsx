d81ff6382b49136e63659decc1d5e8e52e7685fb
VriaA <oyelolaabimbola23@gmail.com>
2024-10-30 02:59:45 +0100

    animate mobile popup nav menu and close it on click outside

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,39 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import victoria from "@/assets/victoria.webp"
 import logos from "@/data/logos"
 import { useViewport } from "@/hooks/useviewport"
 import { Link } from "react-router-dom"
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
 
 export default function Header(): JSX.Element {
     const [isMobileNavMenuOpen, setIsMobileNavMenuOpen] = useState<boolean>(false)
     const [viewport] = useViewport()
     const canShowNav = isMobileNavMenuOpen || viewport.isPc
+    const popUpMobileMenuRef = useRef<HTMLElement | null>(null)
+    const popUpMenuBtnRef = useRef<HTMLButtonElement | null>(null)
+
+    useGSAP(() => {
+        const popUpMobileMenu = popUpMobileMenuRef.current
+        if (canShowNav && popUpMobileMenu) {
+            gsap.fromTo(popUpMobileMenu, { opacity: 0, y: 20 }, { opacity: 1, y: 0, duration: .4, ease: "sine.out" })
+        }
+    }, { dependencies: [canShowNav], revertOnUpdate: true })
+
+    useEffect(() => {
+        const popUpMobileMenu = popUpMobileMenuRef.current
+        const popUpMenuBtn = popUpMenuBtnRef.current
+
+        function closePopUpOnClickOutside(e: MouseEvent): void {
+            if (canShowNav && popUpMobileMenu && popUpMenuBtn) {
+                const clickedElement = e.target as HTMLElement
+                const isClickOutsidePopup = !popUpMobileMenu?.contains(clickedElement) && !popUpMenuBtn?.contains(clickedElement)
+                if (isClickOutsidePopup && isMobileNavMenuOpen) setIsMobileNavMenuOpen(() => false)
+            }
+        }
+        document.addEventListener('click', closePopUpOnClickOutside)
+        return () => document.removeEventListener('click', closePopUpOnClickOutside)
+    }, [canShowNav, isMobileNavMenuOpen])
 
     function toggleMobileNavMenuVisibility(): void {
         if (viewport.isPc) return
@@ -19,7 +45,7 @@ export default function Header(): JSX.Element {
             <img className="object-top" src={victoria} width={72} height={89.92} alt="Oyelola victoria" />
         </Link>
 
-        {canShowNav && <nav className="absolute right-5 md:right-20 top-[84%] lg:static bg-[#33333330] backdrop-blur-[2px] rounded-lg shadow-container border border-white/30">
+        {canShowNav && <nav ref={popUpMobileMenuRef} className="absolute right-5 md:right-20 top-[84%] lg:static bg-[#33333330] backdrop-blur-[2px] rounded-lg shadow-container border border-white/30">
             <ul className="flex flex-col lg:flex-row gap-4 md:gap-8 p-4 md:px-8 md:py-5">
                 <li><Link to="/projects" className="font-satoshi text-base text-off-white metallic-border hover:bg-cta-primary hover:bg-clip-text hover:text-transparent font-medium !leading-none tracking-[.01em]">Projects</Link></li>
                 <li><Link to=".#experience" className="font-satoshi text-base text-off-white metallic-border hover:bg-cta-primary hover:bg-clip-text hover:text-transparent font-medium !leading-none tracking-[.01em]">Experience</Link></li>
@@ -36,7 +62,7 @@ export default function Header(): JSX.Element {
                 </ul>
             </nav>
 
-            <button type="button" className="flex flex-col gap-[5px] lg:hidden" onClick={toggleMobileNavMenuVisibility} aria-label="Navigation menu button">
+            <button type="button" ref={popUpMenuBtnRef} className="flex flex-col gap-[5px] lg:hidden" onClick={toggleMobileNavMenuVisibility} aria-label="Navigation menu button">
                 <span className="w-5 h-[3px] bg-off-white rounded"></span>
                 <span className="w-5 h-[3px] bg-off-white rounded"></span>
                 <span className="w-5 h-[3px] bg-off-white rounded"></span>
