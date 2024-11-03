b89514e146509c638438e563945f8106ff60ae0a
VriaA <oyelolaabimbola23@gmail.com>
2024-11-03 15:48:36 +0100

    add delay to hero animation

--- src/hooks/gsap_animations/useAnimateHomeHero.tsx
+++ src/hooks/gsap_animations/useAnimateHomeHero.tsx
@@ -4,7 +4,7 @@ import { useGSAP } from "@gsap/react"
 export default function useAnimateHomeHero(): void {
 
     useGSAP(() => {
-        const heroTl = gsap.timeline()
+        const heroTl = gsap.timeline({ delay: .5 })
         heroTl.to('.hero-element', { y: 0, opacity: 1, duration: .5, stagger: .2, ease: "back.out" })
         heroTl.to('.name', { y: 0, filter: "blur(0)", opacity: 1, ease: "back.out", duration: '1', stagger: .2 }, "-=.2")
         heroTl.to('#work-status', { opacity: 1, ease: "back.out", duration: 2 }, "+=.2")
