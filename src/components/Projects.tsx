434086d47032e5aebbf1ec8df3decc12ff249e76
Oyelola Victoria <oyelolaabimbola23@gmail.com>
2024-12-01 13:32:33 +0100

    do not display github links for freelance projects
    Signed-off-by: Oyelola Victoria <oyelolaabimbola23@gmail.com>

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -21,7 +21,7 @@ export default function Projects({ projects }: { projects: Project[] }): JSX.Ele
 
             <div className="flex flex-col gap-5 lg:gap-10 mt-5 md:mt-10 w-full lg:flex-row lg:flex-wrap xl:flex-row ">
                 {projects.map((project, i) => {
-                    const { name, description, technologiesUsed, imageSrc, links, isCompleted, isOpenSource } = project
+                    const { name, description, technologiesUsed, imageSrc, links, isCompleted, isOpenSource, isFreelance } = project
                     return (
                         <div key={`project-${i + 1}`} className="flex flex-col items-center lg:w-[calc(50%-20px)] xl:w-full bg-metallic-gradient p-[1px] rounded-2xl overflow-hidden">
                             <div className="flex flex-col xl:flex-row gap-10 items-center xl:items-end xl:justify-between w-full h-full bg-black rounded-2xl px-5 md:px-10 py-10">
@@ -43,19 +43,20 @@ export default function Projects({ projects }: { projects: Project[] }): JSX.Ele
                                                     </svg>
                                                 </a>
                                             }
-
-                                            <a className="w-fit" href={links.github} target="_blank" aria-label={`Link to ${name} GItHub repositoty.`}>
-                                                <svg className="w-7 h-7 xl:w-8 xl:h-8" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                    <g clipPath="url(#clip0_910_44)">
-                                                        <path fillRule="evenodd" clipRule="evenodd" d="M24.0199 0C10.7375 0 0 10.8167 0 24.1983C0 34.895 6.87988 43.9495 16.4241 47.1542C17.6174 47.3951 18.0545 46.6335 18.0545 45.9929C18.0545 45.4319 18.0151 43.509 18.0151 41.5055C11.3334 42.948 9.94198 38.6209 9.94198 38.6209C8.86818 35.8164 7.27715 35.0956 7.27715 35.0956C5.09022 33.6132 7.43645 33.6132 7.43645 33.6132C9.86233 33.7735 11.1353 36.0971 11.1353 36.0971C13.2824 39.7827 16.7422 38.7413 18.1341 38.1002C18.3328 36.5377 18.9695 35.456 19.6455 34.8552C14.3163 34.2942 8.70937 32.211 8.70937 22.9161C8.70937 20.2719 9.66321 18.1086 11.1746 16.4261C10.9361 15.8253 10.1008 13.3409 11.4135 10.0157C11.4135 10.0157 13.4417 9.3746 18.0146 12.4996C19.9725 11.9699 21.9916 11.7005 24.0199 11.6982C26.048 11.6982 28.1154 11.979 30.0246 12.4996C34.5981 9.3746 36.6262 10.0157 36.6262 10.0157C37.9389 13.3409 37.1031 15.8253 36.8646 16.4261C38.4158 18.1086 39.3303 20.2719 39.3303 22.9161C39.3303 32.211 33.7234 34.2539 28.3544 34.8552C29.2296 35.6163 29.9848 37.0583 29.9848 39.3421C29.9848 42.5871 29.9454 45.1915 29.9454 45.9924C29.9454 46.6335 30.383 47.3951 31.5758 47.1547C41.12 43.9491 47.9999 34.895 47.9999 24.1983C48.0392 10.8167 37.2624 0 24.0199 0Z" fill="white" />
-                                                    </g>
-                                                    <defs>
-                                                        <clipPath id="clip0_910_44">
-                                                            <rect width="48" height="48" fill="white" />
-                                                        </clipPath>
-                                                    </defs>
-                                                </svg>
-                                            </a>
+                                            {!isFreelance && 
+                                                <a className="w-fit" href={links.github} target="_blank" aria-label={`Link to ${name} GItHub repositoty.`}>
+                                                    <svg className="w-7 h-7 xl:w-8 xl:h-8" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <g clipPath="url(#clip0_910_44)">
+                                                            <path fillRule="evenodd" clipRule="evenodd" d="M24.0199 0C10.7375 0 0 10.8167 0 24.1983C0 34.895 6.87988 43.9495 16.4241 47.1542C17.6174 47.3951 18.0545 46.6335 18.0545 45.9929C18.0545 45.4319 18.0151 43.509 18.0151 41.5055C11.3334 42.948 9.94198 38.6209 9.94198 38.6209C8.86818 35.8164 7.27715 35.0956 7.27715 35.0956C5.09022 33.6132 7.43645 33.6132 7.43645 33.6132C9.86233 33.7735 11.1353 36.0971 11.1353 36.0971C13.2824 39.7827 16.7422 38.7413 18.1341 38.1002C18.3328 36.5377 18.9695 35.456 19.6455 34.8552C14.3163 34.2942 8.70937 32.211 8.70937 22.9161C8.70937 20.2719 9.66321 18.1086 11.1746 16.4261C10.9361 15.8253 10.1008 13.3409 11.4135 10.0157C11.4135 10.0157 13.4417 9.3746 18.0146 12.4996C19.9725 11.9699 21.9916 11.7005 24.0199 11.6982C26.048 11.6982 28.1154 11.979 30.0246 12.4996C34.5981 9.3746 36.6262 10.0157 36.6262 10.0157C37.9389 13.3409 37.1031 15.8253 36.8646 16.4261C38.4158 18.1086 39.3303 20.2719 39.3303 22.9161C39.3303 32.211 33.7234 34.2539 28.3544 34.8552C29.2296 35.6163 29.9848 37.0583 29.9848 39.3421C29.9848 42.5871 29.9454 45.1915 29.9454 45.9924C29.9454 46.6335 30.383 47.3951 31.5758 47.1547C41.12 43.9491 47.9999 34.895 47.9999 24.1983C48.0392 10.8167 37.2624 0 24.0199 0Z" fill="white" />
+                                                        </g>
+                                                        <defs>
+                                                            <clipPath id="clip0_910_44">
+                                                                <rect width="48" height="48" fill="white" />
+                                                            </clipPath>
+                                                        </defs>
+                                                    </svg>
+                                                </a>
+                                            }
                                         </div>
                                         : <p className="flex items-center gap-4 h-fit font-satoshi font-medium text-sm lg:text-base tracking-[.01em] bg-metallic-text text-transparent bg-clip-text leading-[100%]">
                                             <GlowingDot style="bg-[#FF0000]" />
