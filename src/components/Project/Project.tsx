272bf4eac2421e65ddd7a6f9c7da393bbdd79347
VriaA <oyelolaabimbola23@gmail.com>
2024-12-25 13:26:37 +0100

    adjust project styles at the 1024px breakpoint

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -32,18 +32,18 @@ export default function Project({
     return (
         <div
             key={`project-${index + 1}`}
-            className={`${POSITION_CLASS} flex-none flex flex-col items-center lg:w-[calc(50%-20px)] xl:w-full bg-metallic-gradient p-[1px] rounded-2xl overflow-hidden`}
+            className={`${POSITION_CLASS} flex-none flex flex-col items-center w-full h-fit bg-metallic-gradient p-[1px] rounded-2xl overflow-hidden`}
             style={{ top: top }}>
-            <div className="flex flex-col xl:flex-row gap-10 items-center xl:items-end xl:justify-between w-full h-full bg-black rounded-2xl px-5 md:px-10 py-10">
+            <div className="flex flex-col lg:flex-row gap-10 items-center xl:items-end lg:justify-between w-full h-full bg-black rounded-2xl px-5 md:px-10 py-10">
                 <img
                     className={`${
                         !isCompleted ? "blur-md" : ""
-                    } order-0 xl:order-2 w-[300px] lg:w-[540px] h-fit`}
+                    } order-0 lg:order-2 w-[300px] lg:w-1/2 xl:w-[540px] h-fit`}
                     src={imageSrc}
                     alt={`Oyelola Victoria's ${name} Project`}
                 />
 
-                <div className="order-1 xl:order-0 flex flex-col gap-10 xl:gap-20 justify-between min-h-full w-full xl:w-[50%]">
+                <div className="order-1 xl:order-0 flex flex-col gap-10 lg:gap-20 justify-between min-h-full w-full lg:w-[50%]">
                     {isCompleted ? (
                         <ProjectLinks
                             links={links}
@@ -52,16 +52,16 @@ export default function Project({
                             isTeam={isTeam}
                         />
                     ) : (
-                        <p className="flex items-center gap-4 h-fit font-satoshi font-medium text-sm lg:text-base tracking-[.01em] bg-metallic-text text-transparent bg-clip-text leading-[100%]">
+                        <p className="flex items-center gap-4 h-fit font-satoshi font-medium text-sm xl:text-base tracking-[.01em] bg-metallic-text text-transparent bg-clip-text leading-[100%]">
                             <GlowingDot style="bg-[#FF0000]" />
                             In progress
                         </p>
                     )}
                     <div>
-                        <h4 className="font-satoshi font-bold text-[28px] lg:text-5xl leading-none -tracking-[.02em]">
+                        <h4 className="font-satoshi font-bold text-[28px] xl:text-5xl leading-none -tracking-[.02em]">
                             {name}
                         </h4>
-                        <p className="bg-metallic-text text-transparent bg-clip-text mt-3 font-satoshi font-medium text-sm lg:text-lg leading-[150%] tracking-[.01em]">
+                        <p className="bg-metallic-text text-transparent bg-clip-text mt-3 font-satoshi font-medium text-sm xl:text-lg leading-[150%] tracking-[.01em]">
                             {description}
                         </p>
 
@@ -71,7 +71,7 @@ export default function Project({
                                 return (
                                     <p
                                         key={`${name}-${techName}`}
-                                        className="project-stack flex items-center gap-1 w-fit px-2 py-1 xl:px-4 xl:py-2 h-8 xl:h-9 font-satoshi font-medium text-sm lg:text-base tracking-[.01em] bg-metallic-text text-transparent bg-clip-text !leading-none rounded-2xl border bg-dark-gray/20 border-white/30 shadow-container">
+                                        className="project-stack flex items-center gap-1 w-fit px-2 py-1 xl:px-4 xl:py-2 h-8 xl:h-9 font-satoshi font-medium text-sm xl:text-base tracking-[.01em] bg-metallic-text text-transparent bg-clip-text !leading-none rounded-2xl border bg-dark-gray/20 border-white/30 shadow-container">
                                         {logo}
                                         {techName}
                                     </p>
