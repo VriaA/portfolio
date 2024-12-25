0af48ea4ffe9b953d462ec8c22eed84ccb907511
VriaA <oyelolaabimbola23@gmail.com>
2024-12-25 17:41:41 +0100

    update skills and adjust skills section style

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -13,17 +13,18 @@ export default function Skills(): JSX.Element {
                 My Expertise
             </h3>
             <div className="w-full mt-5 md:mt-10 bg-metallic-gradient p-[1px] rounded-2xl overflow-hidden">
-                <div className="flex flex-col items-center justify-center bg-black rounded-2xl py-5 md:py-20 px-0">
-                    <div className="flex justify-center flex-wrap max-w-[335px] md:max-w-[1019px]">
+                <div className="flex flex-col items-center justify-center bg-black rounded-2xl py-5 md:py-10 px-5 md:px-10">
+                    <div className="flex justify-center flex-wrap max-w-[335px] md:max-w-none">
                         {skills.map((skill, i) => {
                             const RIGHT_BORDER =
-                                (i + 1) % 5 !== 0 && "xl:border-r";
+                                (i + 1) % 6 !== 0 && "xl:border-r";
+                            const LEFT_BORDER = i === 12 && "xl:border-l";
                             const BOTTOM_BORDER =
-                                i < skills.length - 5 && "xl:border-b";
+                                i < skills.length - 4 && "xl:border-b";
                             return (
                                 <p
                                     key={`skill-${i}`}
-                                    className={`skill flex justify-center items-center gap-2 w-16 h-16 md:w-[200px] md:h-24 text-base xl:text-lg font-satoshi font-medium !leading-none tracking-[.01em] bg-metallic-text text-transparent bg-clip-text metallic-border ${RIGHT_BORDER} ${BOTTOM_BORDER}`}>
+                                    className={`skill flex justify-center items-center gap-2 w-16 h-16 md:w-[200px] md:h-24 text-base xl:text-lg font-satoshi font-medium !leading-none tracking-[.01em] bg-metallic-text text-transparent bg-clip-text metallic-border ${LEFT_BORDER} ${RIGHT_BORDER} ${BOTTOM_BORDER}`}>
                                     {skill.logo}
                                     {!viewport.isMobile && skill.skill}
                                 </p>
--- src/data/skills.tsx
+++ src/data/skills.tsx
@@ -1,66 +1,68 @@
-import logos from "./logos"
+import logos from "./logos";
 
 export default [
-  {
-    skill: "HTML",
-    logo: logos.html
-  },
-  {
-    skill: "CSS",
-    logo: logos.css
-  },
-  {
-    skill: "Tailwind CSS",
-    logo: logos.tailwindcss
-  },
-  {
-    skill: "Material UI",
-    logo: logos.mui
-  },
-  {
-    skill: "Rest API",
-    logo: logos.restapi
-
-  },
-  {
-    skill: "JavaScript",
-    logo: logos.javascript
-
-  },
-  {
-    skill: "TypeScript",
-    logo: logos.typescript
-  },
-  {
-    skill: "React",
-    logo: logos.react
-  },
-  {
-    skill: "React Router",
-    logo: logos.reactRouter
-  },
-  {
-    skill: "Next.js",
-    logo: logos.nextjs
-  },
-  {
-    skill: "Firebase",
-    logo: logos.firebase
-  },
-  {
-    skill: "Jest",
-    logo: logos.jest
-  },
-  {
-    skill: "",
-    logo: logos.gsap
-  },
-  {
-    skill: "Git",
-    logo: logos.git
-  },
-  {
-    skill: "Figma",
-    logo: logos.figma
-  }
-]
+    {
+        skill: "HTML",
+        logo: logos.html,
+    },
+    {
+        skill: "CSS",
+        logo: logos.css,
+    },
+    {
+        skill: "Tailwind CSS",
+        logo: logos.tailwindcss,
+    },
+    {
+        skill: "",
+        logo: logos.sassFull,
+    },
+    {
+        skill: "Material UI",
+        logo: logos.mui,
+    },
+    {
+        skill: "Rest API",
+        logo: logos.restapi,
+    },
+    {
+        skill: "JavaScript",
+        logo: logos.javascript,
+    },
+    {
+        skill: "TypeScript",
+        logo: logos.typescript,
+    },
+    {
+        skill: "React",
+        logo: logos.react,
+    },
+    {
+        skill: "React Router",
+        logo: logos.reactRouter,
+    },
+    {
+        skill: "Next.js",
+        logo: logos.nextjs,
+    },
+    {
+        skill: "Firebase",
+        logo: logos.firebase,
+    },
+    {
+        skill: "Jest",
+        logo: logos.jest,
+    },
+    {
+        skill: "",
+        logo: logos.gsap,
+    },
+    {
+        skill: "Git",
+        logo: logos.git,
+    },
+    {
+        skill: "Figma",
+        logo: logos.figma,
+    },
+];
