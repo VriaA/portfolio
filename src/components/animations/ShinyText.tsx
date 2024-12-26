2f7d5799c02627fb72d49df462a648b36d65d965
VriaA <oyelolaabimbola23@gmail.com>
2024-12-26 01:28:34 +0100

    change div to span in ShinyText component

--- src/components/animations/ShinyText.tsx
+++ src/components/animations/ShinyText.tsx
@@ -2,23 +2,30 @@ type ShinyTextProps = {
     text: string;
     disabled?: boolean;
     speed?: number;
-    className?: string
-}
+    className?: string;
+};
 
-export default function ShinyText({ text, disabled = false, speed = 5, className = '' }: ShinyTextProps): JSX.Element {
+export default function ShinyText({
+    text,
+    disabled = false,
+    speed = 5,
+    className = "",
+}: ShinyTextProps): JSX.Element {
     const animationDuration = `${speed}s`;
 
     return (
-        <div
-            className={`text-[#b5b5b5a4] bg-clip-text inline-block ${disabled ? '' : 'animate-shine'} ${className}`}
+        <span
+            className={`text-[#b5b5b5a4] bg-clip-text inline-block ${
+                disabled ? "" : "animate-shine"
+            } ${className}`}
             style={{
-                backgroundImage: 'linear-gradient(90deg, #4b54ff, #BBB999, #4b54ff)',
-                backgroundSize: '200% 100%',
-                WebkitBackgroundClip: 'text',
+                backgroundImage:
+                    "linear-gradient(90deg, #4b54ff, #BBB999, #4b54ff)",
+                backgroundSize: "200% 100%",
+                WebkitBackgroundClip: "text",
                 animationDuration: animationDuration,
-            }}
-        >
+            }}>
             {text}
-        </div>
+        </span>
     );
-};
+}
