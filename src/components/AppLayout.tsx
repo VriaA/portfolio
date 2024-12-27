36bc6240820e040f10b8ab914bc241d77221488d
VriaA <oyelolaabimbola23@gmail.com>
2024-12-27 03:20:20 +0100

    implement smooth scrolling with Lenis

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/Footer";
 import useScrollToSectionOnHashChange from "@/hooks/useScrollToSectionOnHashChange";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
+import { ReactLenis } from "lenis/react";
 
 export default function AppLayout() {
     const location = useLocation();
@@ -18,15 +19,17 @@ export default function AppLayout() {
     }, [location]);
 
     return (
-        <div className="relative flex flex-col items-center w-full bg-black px-5 md:px-20">
-            <div className="absolute inset-0 mx-auto z-[1] left-[-1px] top-[-1px] w-full h-full site-bg"></div>
-            <AppContextProvider>
-                <Header />
-                <main className="flex flex-col pt-20 w-full max-w-[1440px]">
-                    <Outlet />
-                </main>
-                <Footer />
-            </AppContextProvider>
-        </div>
+        <ReactLenis root options={{ duration: 0.7 }}>
+            <div className="relative flex flex-col items-center w-full bg-black px-5 md:px-20">
+                <div className="absolute inset-0 mx-auto z-[1] left-[-1px] top-[-1px] w-full h-full site-bg"></div>
+                <AppContextProvider>
+                    <Header />
+                    <main className="flex flex-col pt-20 w-full max-w-[1440px]">
+                        <Outlet />
+                    </main>
+                    <Footer />
+                </AppContextProvider>
+            </div>
+        </ReactLenis>
     );
 }
