52811ea98912f4b354c4eb8ff7f3c5ae11bd08e4
VriaA <oyelolaabimbola23@gmail.com>
2024-12-29 04:03:17 +0100

    animate section headings

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -1,6 +1,6 @@
 export default function SectionHeader({ text }: { text: string }): JSX.Element {
     return (
-        <h2 className="px-8 py-2 w-fit h-fit font-satoshi font-medium text-base xl:text-lg tracking-[.01em] bg-metallic-text text-transparent bg-clip-text !leading-none rounded-2xl border bg-dark-gray/20 border-white/30 shadow-container">
+        <h2 className="heading px-8 py-2 w-fit h-fit font-satoshi font-medium text-base xl:text-lg tracking-[.01em] bg-metallic-text text-transparent bg-clip-text !leading-none rounded-2xl border bg-dark-gray/20 border-white/30 shadow-container">
             {text}
         </h2>
     );
